680b2670b9015ee231cd0452e86586ed12a17961
jacopoUCL <jacopo.palombarini@studio.unibo.it>
2025-12-01 16:34:01 +0100

    report update

--- script.R
+++ script.R
@@ -49,6 +49,7 @@ Ther <- Ther[primaryid %in% Demo$primaryid]
 # C = Concomitant - PS = Principal suspect - SS = Secondary suspect - I = Interacting
 pids_inter <- unique(Drug[role_cod == "I"]$primaryid) # 95.947
 pids_non_inter <- unique(Drug[role_cod != "I"]$primaryid) # 14.739.289
+subs_inter <- unique(Drug[role_cod != "I"]$substance) # 6317
 # Unique combinations by primaryid and role_cod for all data
 write.xlsx(
   Drug[, .SD[!duplicated(Drug, by = c("primaryid", "role_cod"))]]
@@ -208,40 +209,36 @@ p2 <- ggplot(bars_2, aes(x = total_drugs, y = count, fill = role_cod)) +
                      expand = expansion(mult = c(0, 0.02))) +
   scale_y_continuous(breaks = seq(0, y_top_2, by = step_guess),
                      limits = c(0, y_top_2 + step_guess * 0.25)) +
-  scale_fill_manual(values = c("I" = "steelblue", "S" = "lightgreen", "C" = "orange")) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  # segment percentages (per role)
-  geom_text(
-    aes(label = paste0(perc, "%")),
-    position = position_stack(vjust = 0.5),
-    size = 3,
-    color = "white"
-  ) +
-  # TOTAL percentage per bar (top label) — no inherited aes
-  geom_text(
-    data = totals_2,
-    mapping = aes(x = total_drugs, y = count_total, label = paste0(perc_total, "%")),
-    vjust = -0.3,
-    size = 3.5,
-    fontface = "bold",
-    color = "black",
-    inherit.aes = FALSE
-  ) +
-  # Top-right annotation using freq_roles
-  annotate(
-    "text",
-    x = 19.5, y = y_top_2 * 0.75,
-    label = paste0(
-      "Within total reports with at least one I: ",
-      "\n- At least one I: ", 
-      formatC(getN("I"), format = "d", big.mark = ","),
-      "\n- At least one S: ",
-      formatC(getN("S"), format = "d", big.mark = ","),
-      "\n- At least one C: ",
-      formatC(getN("C"), format = "d", big.mark = ",")
-    ), hjust = 0, size = 3.5, color = "black"
-  )
+  scale_fill_manual(values = c("I" = "#BBDAF8", "S" = "#90C893", "C" = "#FFC899")) +
+  geom_text(aes(label = paste0(perc, "%")),
+            position = position_stack(vjust = 0.5),
+            size = 3,
+            color = "white") +
+  geom_text(data = totals_2,
+            mapping = aes(x = total_drugs, y = count_total, label = paste0(perc_total, "%")),
+            vjust = -0.3,
+            size = 3.5,
+            fontface = "bold",
+            color = "black",
+            inherit.aes = FALSE) +
+  theme_void() +
+  theme(axis.title.y  = element_text(size = 10, face = "bold", angle = 90),
+    axis.title.x  = element_text(size = 10, face = "bold"),
+    axis.text   = element_text(size = 10, face = "bold"),
+    axis.text.x = element_text(size = 10, face = "bold", angle = 45, hjust = 1, vjust = 1),
+    axis.text.y = element_text(size = 10, face = "bold", angle = 45, hjust = 1.5),
+    plot.title  = element_text(size = 10, face = "bold")) # +
+  # annotate("text",
+  #   x = 19.5, y = y_top_2 * 0.75,
+  #   label = paste0(
+  #     "Within total reports with at least one I: ",
+  #     "\n- At least one I: ", 
+  #     formatC(getN("I"), format = "d", big.mark = ","),
+  #     "\n- At least one S: ",
+  #     formatC(getN("S"), format = "d", big.mark = ","),
+  #     "\n- At least one C: ",
+  #     formatC(getN("C"), format = "d", big.mark = ",")), 
+  #   hjust = 0, size = 3.5, color = "black")
 
 p1 / p2 + plot_layout(heights = c(1, 1.2)) + 
   plot_annotation(title = "Reports by number of reported drugs", 
@@ -252,21 +249,28 @@ p3 <- ggplot(bars_2, aes(x = total_drugs, y = perc,
                    group = role_cod, color = role_cod)) +
   geom_line(linewidth = 1.2) +
   geom_point(size = 3) +
-  scale_x_continuous(breaks = 2:35, limits = c(2, 35),
+  scale_x_continuous(breaks = 1:35, limits = c(1, 35),
                      expand = expansion(mult = c(0, 0.02))) +
   scale_y_continuous(breaks = seq(0, 60, by = 5),
                      limits = c(0, 60)) +
-  labs(x = "Number of drugs (S (PS or SS) or I or C)",
+  labs(x = "Number of drugs - S (PS or SS) or I or C -",
        y = "% by drug role", color = "Drug role") +
-  scale_color_manual(values = c("I" = "steelblue",
-                                "S" = "lightgreen", 
-                                "C" = "orange")) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  scale_color_manual(values = c("I" = "#BBDAF8",
+                                "S" = "#90C893", 
+                                "C" = "#FFC899")) +
+  theme_void() +
+  theme(axis.title.y  = element_text(size = 10, face = "bold", angle = 90),
+        axis.title.x  = element_text(size = 10, face = "bold"),
+        axis.text   = element_text(size = 10, face = "bold"),
+        axis.text.x = element_text(size = 10, face = "bold", angle = 45, hjust = 1, vjust = 1.5),
+        axis.text.y = element_text(size = 10, face = "bold", angle = 45, hjust = 1.5),
+        plot.title  = element_text(size = 10, face = "bold"))
 
 p2 / p3 + plot_layout(heights = c(1, 1.2)) + 
   plot_annotation(title = "Reports by number of reported drugs and roles", 
                   subtitle = "(1986-2024, Reports with at least one interacting drug (I))")
+
+ggsave("results/plots/plot_count.tiff", plot = p2 / p3, width = 14, height = 10, units = "in", dpi = 1000, compression = "lzw")
 
 # Descriptive - all data -----
 descriptive(pids_non_inter, file_name = "results/Descriptive_all_data_non_inter.xlsx")
@@ -489,8 +493,8 @@ clean_desc <- function(path){
 }
 
 files <- c(
-  int = "results/Descriptive_all_data_inter.xlsx",
-  non_int = "results/Descriptive_all_data_non_inter.xlsx"
+  int = "results/tables/univariate/Descriptive_all_data_inter.xlsx",
+  non_int = "results/tables/univariate/Descriptive_all_data_non_inter.xlsx"
 )
 
 dfs <- lapply(files, clean_desc)
@@ -605,28 +609,32 @@ plot_residuals <- function(resid_mat, title) {
     geom_text(aes(label = round(Residual, 1))) +
     scale_fill_gradient2(
       midpoint = 0,
-      low = "blue",
-      mid = "white",
-      high = "red"
-    ) +
+      low = "#BBDAF8",
+      mid = "#FFFFFF",
+      high = "#F4BED4",
+      limits = c(min(df_long$Residual), max(df_long$Residual))) +
     labs(
       title = title,
       x = NULL,
       y = NULL,
-      fill = "Std. residual"
-    ) +
-    theme_minimal() +
+      fill = "Std. residual") +
+    theme_void() +
     theme(
-      axis.text.x = element_text(angle = 45, hjust = 1)
-    )
+      axis.title.y  = element_text(size = 10, face = "bold", angle = 90),
+      axis.title.x  = element_text(size = 10, face = "bold"),
+      axis.text   = element_text(size = 10, face = "bold"),
+      axis.text.x = element_text(size = 10, face = "bold", angle = 45, hjust = 1, vjust = 1),
+      axis.text.y = element_text(size = 10, face = "bold", angle = 45, hjust = 1),
+      plot.title  = element_text(size = 10, face = "bold"))
+  
 }
 fig_all <- plot_residuals(test_sex$residuals, "Sex") + 
   plot_residuals(test_age$residuals, "Age") +
   plot_residuals(test_out$residuals, "Outcome") +
   plot_residuals(test_con$residuals, "Continent") +
   plot_layout(ncol = 2)
-fig_all + plot_annotation(title = "Standardized Residuals Across Key Variables")
-ggsave("results/plots/residuals_all.png", fig_all, width = 12, height = 10, dpi = 300)
+p_chisq <- fig_all
+ggsave("results/plots/plot_chisq.tiff", plot = p_chisq, width = 17, height = 13, units = "in", dpi = 1000, compression = "lzw")
 
 
 # Cramer's V of imbalance
@@ -672,7 +680,6 @@ add_chi_sheet(wb, "Age", test_age, t(tab_age))
 add_chi_sheet(wb, "Outcome", test_out, t(tab_out))
 add_chi_sheet(wb, "Continent", test_con, t(tab_con))
 saveWorkbook(wb, "results/chi_tables.xlsx", overwrite = TRUE)
-
 
 ## ADR -----
 # Count ADR repetitions for interacting drugs ---------------------
@@ -840,7 +847,6 @@ addWorksheet(wb, "Indications")
 row_counter <- 1
 drug_names <- names(int_indi)
 for (i in seq_along(int_indi)) {
-  # Write drug name
   writeData(wb, "Indications", paste("Drug:", drug_names[i]), startRow = row_counter, startCol = 1)
   row_counter <- row_counter + 1
   
@@ -850,21 +856,17 @@ for (i in seq_along(int_indi)) {
     next
   }
   
-  # Ensure nice output: convert factor to character and sort by N desc
   out <- as.data.frame(dt)
   out$indi_pt <- as.character(out$indi_pt)
   out <- out[order(out$N, decreasing = TRUE), , drop = FALSE]
   
-  # Write the table
   writeData(wb, "Indications", out, startRow = row_counter, startCol = 1, withFilter = TRUE)
   
-  # Optional: auto width for the two columns just written
   setColWidths(wb, "Indications", cols = 1:ncol(out), widths = "auto")
   
-  # Advance the row counter (+2 for spacing)
   row_counter <- row_counter + nrow(out) + 2
 }
-saveWorkbook(wb, "results/Most_reported_interacfting_drugs_most_reported_indications.xlsx", overwrite = TRUE)
+saveWorkbook(wb, "results/tables/rankings/Most_reported_indications_for_most_reported_interacting_drugs.xlsx", overwrite = TRUE)
 
 # Plot for interacting drugs by classes -----
 # Interacting drugs count per substance
@@ -1289,7 +1291,7 @@ p4 <- ggplot(strata_df_imm, aes(x = Class2, y = N_sub, fill = substance)) +
 ggsave("results/plots/plot5.tiff", plot = (p1 | p2) / (p3 | p4), width = 22, height = 18, units = "in", dpi = 1000, compression = "lzw")
 
 # Plot trend for most reported interacting drugs -----
-int_data <- read_excel("results/Most_reported_interacting_drugs.xlsx")
+int_data <- read_excel("results/tables/rankings/Most_reported_interacting_drugs.xlsx")
 years_ <- function(path = NA) {
   d <- read_excel(path)
   d <- t(d)
@@ -1299,60 +1301,76 @@ years_ <- function(path = NA) {
   colnames(d) <- "N"
   return(d)
 }
-data_war <- years_("results/Descriptive_warfarin.xlsx")
-data_tac <- years_("results/Descriptive_tacrolimus.xlsx")
-data_ace <- years_("results/Descriptive_acetylsalicylic_acid.xlsx")
-data_que <- years_("results/Descriptive_quetiapine.xlsx")
-data_val <- years_("results/Descriptive_valproic_acid.xlsx")
-data_riv <- years_("results/Descriptive_rivaroxaban.xlsx")
-data_par <- years_("results/Descriptive_paracetamol.xlsx")
-data_rit <- years_("results/Descriptive_ritonavir.xlsx")
-data_clo <- years_("results/Descriptive_clozapine.xlsx")
-data_fur <- years_("results/Descriptive_furosemide_acid.xlsx")
+data_war <- years_("results/tables/univariate/Descriptive_warfarin.xlsx")
+data_tac <- years_("results/tables/univariate/Descriptive_tacrolimus.xlsx")
+data_ace <- years_("results/tables/univariate/Descriptive_acetylsalicylic_acid.xlsx")
+data_que <- years_("results/tables/univariate/Descriptive_quetiapine.xlsx")
+data_val <- years_("results/tables/univariate/Descriptive_valproic_acid.xlsx")
+data_all_int <- years_("results/tables/univariate/Descriptive_all_data_inter.xlsx")
+# data_riv <- years_("results/tables/univariate/Descriptive_rivaroxaban.xlsx")
+# data_par <- years_("results/tables/univariate/Descriptive_paracetamol.xlsx")
+# data_rit <- years_("results/tables/univariate/Descriptive_ritonavir.xlsx")
+# data_clo <- years_("results/tables/univariate/Descriptive_clozapine.xlsx")
+# data_fur <- years_("results/tables/univariate/Descriptive_furosemide_acid.xlsx")
 
-df <- cbind(data_war, data_tac, data_ace, data_que, data_val, 
-            data_riv, data_par, data_rit, data_clo, data_fur)
-colnames(df) <- int_data$substance
-
+df <- cbind(data_war, data_tac, data_ace, data_que, data_val) #, data_riv, data_par, data_rit, data_clo, data_fur
+colnames(df) <- c(int_data$substance[1:5])
 df <- df %>%
-  rownames_to_column(var = "year")
+  rownames_to_column(var = "year") %>%
+  mutate(
+    across(
+      -year,
+      ~ as.numeric(gsub(",", "", as.character(.x)))
+    )
+  ) %>% 
+  mutate(Avg. = rowSums(across(where(is.numeric))))
+
 df$year <- as.numeric(df$year)
+df$Avg. <- df$Avg./5
 
 df_long <- df %>%
   pivot_longer(
     cols = -year,
-    names_to = "drug",
+    names_to  = "drug",
     values_to = "value"
-  ) %>%
-  mutate(value = as.numeric(value))
+  )
 
-ggplot(df_long, aes(x = year, y = value, color = drug, group = drug)) +
-  geom_line(size = 1) +
-  geom_point(size = 2) +
+pastel_palette2 <- c("#529FE4", "red", "#9BD9F0", "#6DB671", "#FFAF70", "#EC98BC")
+
+alphas <- c(rep(0.4, 5), 1)
+names(alphas) <- unique(df_long$drug)
+
+py <- ggplot(df_long, aes(x = year, y = value, group = drug)) +
+  geom_line(aes(color = drug, alpha = drug), size = 1.3) +
+  geom_point(
+    aes(alpha = drug),
+    size = 2.5,
+    stroke = 1,
+    fill = "grey") +
+  geom_point(
+    aes(alpha = drug, color = drug),
+    size = 1.7,
+    stroke = 1,
+    fill = "white") +
   theme_minimal(base_size = 14) +
   labs(
     title = "Drug values over years",
     x = "Year",
     y = "Value",
-    color = "Drug"
-  ) +
-  scale_x_continuous(
-    breaks = unique(df_long$year)
-  ) +
+    color = "Drug") +
+  scale_x_continuous(breaks = unique(df_long$year)) +
+  scale_color_manual(values = pastel_palette2) +
+  scale_alpha_manual(values = alphas, guide = "none") +
+  theme_void() +
   theme(
-    legend.position = "right",
-    # Axis titles
-    axis.title.x = element_text(size = 18, face = "bold"),
-    axis.title.y = element_text(size = 18, face = "bold"),
-    # Axis tick labels
-    axis.text.x = element_text(size = 14, face = "bold", angle = 45, hjust = 1),
-    axis.text.y = element_text(size = 14, face = "bold"),
-    # Legend
-    legend.title = element_text(size = 16, face = "bold"),
-    legend.text = element_text(size = 14, face = "bold"),
-    # Plot title
-    plot.title = element_text(size = 20, face = "bold", hjust = 0.5)
-  )
+    axis.title.y  = element_text(size = 10, face = "bold", angle = 90),
+    axis.title.x  = element_text(size = 10, face = "bold"),
+    axis.text     = element_text(size = 10, face = "bold"),
+    axis.text.x   = element_text(size = 10, face = "bold", angle = 45, hjust = 1, vjust = 1),
+    axis.text.y   = element_text(size = 10, face = "bold", angle = 45, hjust = 1.5),
+    plot.title    = element_text(size = 10, face = "bold"))
+
+ggsave("results/plots/plot_years.tiff", plot = py, width = 12, height = 8, units = "in", dpi = 1000, compression = "lzw")
 
 #####
 
@@ -1361,7 +1379,7 @@ ggplot(df_long, aes(x = year, y = value, color = drug, group = drug)) +
 ## for the most reported interacting drugs -----
 pids_most_int <- list(pids_1_inter, pids_2_inter, pids_3_inter, pids_4_inter, pids_5_inter, 
                       pids_6_inter, pids_7_inter, pids_8_inter, pids_9_inter, pids_10_inter)
-roles <- c("PS", "SS", "C")
+ggsave("results/plots/plot0.tiff", plot = last_plot(), width = 12, height = 8, units = "in", dpi = 1000, compression = "lzw")roles <- c("PS", "SS", "C")
 tpl <- data.frame(PS = I(list(NULL)),
                   SS = I(list(NULL)),
                   C  = I(list(NULL)))
